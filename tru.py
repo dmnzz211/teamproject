218f0270f75fc0998a57981314ea02439a8710c2
Nguyen Thanh Phuong <thanhphuongn1112007@gmail.com>
2026-08-18 09:18:58 +0700

    Phep tru 2 so

--- tru.py
+++ tru.py
@@ -1,4 +1,4 @@
-a = int(input("Nhập số thứ nhất: "))
-b = int(input("Nhập số thứ hai: "))
+b = int(input("Nhập số thứ nhất: "))
+c = int(input("Nhập số thứ hai: "))
 
-print("Kết quả:", a - b)
+print("Kết quả:", c - b)
